6b800aafb8c2c2a3209b9aa8a88b9af2a4e3c3d1
felipeBatista <actividades.felipebtst@gmail.com>
2024-06-24 13:54:22 -0300

    feat: a new feature

--- app/src/main/java/com/example/mimate/RegisterActivity.java
+++ app/src/main/java/com/example/mimate/RegisterActivity.java
@@ -1,7 +1,6 @@
 package com.example.mimate;
 
-import
-        android.os.Bundle;
+import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
