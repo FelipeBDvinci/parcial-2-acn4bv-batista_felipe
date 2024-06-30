e1234e6b77f9005a47fb97a10d92702ecb27a05e
felipeBatista <actividades.felipebtst@gmail.com>
2024-06-29 22:22:03 -0300

    feat: a new feature

--- app/src/main/java/com/example/mimate/LoginActivity.java
+++ app/src/main/java/com/example/mimate/LoginActivity.java
@@ -1,45 +1,95 @@
 package com.example.mimate;
 
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
-    Button btn_login;
-    EditText email, password;
+    TextView textView;
+    ProgressBar progressBar;
+
+
+
+
+    public void login(String email, String password) {
+
+        Log.i("firebase", "email:" + email);
+        Log.i("firebase", "password: " + password);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            progressBar.setVisibility(View.GONE);
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class );
+                            startActivity (intent);
+
+                        } else {
+                            Toast.makeText(LoginActivity.this, " Falló el login ",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+
+
+    public void onLoginButtonClick (View view) {
+
+        EditText emailInput = findViewById(R.id.correo);
+        EditText passInput = findViewById(R.id.contrasena);
+
+        String email = emailInput.getText(). toString();
+        String password = passInput.getText().toString();
+
+        this.login( email, password);
+
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+        mAuth = FirebaseAuth.getInstance();
+        textView = findViewById(R.id.contrasena);
+        progressBar = this.<ProgressBar>findViewById(R.id.main);
 
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity (intent);
+                finish ();
 
-            email = findViewById(R.id.correo);
-            password = findViewById(R.id.contrasena);
-            btn_login = findViewById(R.id.btn_ingresar);
-
-
-
-
-            return insets;
-
-
-
-
+            }
         });
-
-
     }
+
+
+
+
+
 }
